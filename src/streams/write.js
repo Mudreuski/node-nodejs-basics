a0613de9befde39fb786a089c03d11f79ea8aafd
Henadzi Mudreuski <henadzi.mudreuski@gcore.lu>
2022-06-03 06:26:13 +0300

    feat: add streams tasks solution

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,12 @@
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+import { createWriteStream } from 'fs';
+
 export const write = async () => {
-    // Write your code here 
+    const pathToFile = resolve(dirname(fileURLToPath(import.meta.url)), 'files/fileToWrite.txt');
+    const stream = createWriteStream(pathToFile, 'utf8');
+
+    process.stdin.pipe(stream);
 };
+
+await write();
