4f757a0064ef10d564dffb48ecf2a5ca8af4d7cf
Henadzi Mudreuski <henadzi.mudreuski@gcore.lu>
2022-06-05 21:40:37 +0300

    feat: add cp tasks solution

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,14 @@
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+import { spawn } from 'child_process';
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+    const pathToFile = resolve(dirname(fileURLToPath(import.meta.url)), 'files/script.js');
+    const child = spawn('node', [pathToFile, ...args]);
+
+    process.stdin.pipe(child.stdin);
+
+    child.stdout.on('data', data => console.log(data.toString()));
 };
+
+await spawnChildProcess([42, 404, 500, 2022, 2077]);
